16583c9d90aa92f3f480162b6aada46c85eed059
Julian Carrillo <julian.carrillo.cruz@gmail.com>
2017-07-27 19:37:32 -0500

    Refactors options script.

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -1,30 +1,30 @@
-import ext from "./utils/ext";
-import storage from "./utils/storage";
+import storage from './utils/storage';
 
-var colorSelectors = document.querySelectorAll(".js-radio");
+const colorSelectors = document.querySelectorAll('.js-radio');
 
-var setColor = (color) => {
+const setColor = color => {
   document.body.style.backgroundColor = color;
 };
 
-storage.get('color', function(resp) {
-  var color = resp.color;
-  var option;
-  if(color) {
+storage.get('color', resp => {
+  let color = resp.color;
+  let option;
+
+  if (color) {
     option = document.querySelector(`.js-radio.${color}`);
     setColor(color);
   } else {
-    option = colorSelectors[0]
+    option = colorSelectors[0];
   }
 
-  option.setAttribute("checked", "checked");
+  option.setAttribute('checked', 'checked');
 });
 
-colorSelectors.forEach(function(el) {
-  el.addEventListener("click", function(e) {
-    var value = this.value;
-    storage.set({ color: value }, function() {
+colorSelectors.forEach(el => {
+  el.addEventListener('click', function (e) {
+    let value = this.value;
+    storage.set({color: value}, function () {
       setColor(value);
     });
-  })
-})
+  });
+});
