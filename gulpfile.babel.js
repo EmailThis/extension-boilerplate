e358d5a6f0a089b280bf29ce0f94739d1d2be8d6
Tim Bernhard <tim@bernhard-webstudio.ch>
2019-05-12 17:19:08 +0200

    upgrade dependencies

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,17 @@
-import fs from "fs";
-import { gulp, series, parallel } from 'gulp';
-import { merge } from 'event-stream'
 import browserify from 'browserify';
-import source from 'vinyl-source-stream';
-import buffer from 'vinyl-buffer';
+import {merge} from 'event-stream'
+import fs from 'fs';
+import gulp from 'gulp';
+import gulpif from 'gulp-if';
 import preprocessify from 'preprocessify';
-import gulpif from "gulp-if";
+import buffer from 'vinyl-buffer';
+import source from 'vinyl-source-stream';
 
 const $ = require('gulp-load-plugins')();
 
-var production = process.env.NODE_ENV === "production";
-var target = process.env.TARGET || "chrome";
-var environment = process.env.NODE_ENV || "development";
+var production = process.env.NODE_ENV === 'production';
+var target = process.env.TARGET || 'chrome';
+var environment = process.env.NODE_ENV || 'development';
 
 var generic = JSON.parse(fs.readFileSync(`./config/${environment}.json`));
 var specific = JSON.parse(fs.readFileSync(`./config/${target}.json`));
@@ -19,57 +19,43 @@ var context = Object.assign({}, generic, specific);
 
 var defaultManifest = {
   dev: {
-    "background": {
-      "scripts": [
-        "scripts/livereload.js",
-        "scripts/background.js"
-      ]
-    }
+    'background':
+        {'scripts': ['scripts/livereload.js', 'scripts/background.js']}
   },
 
-  firefox: {
-    "applications": {
-      "gecko": {
-        "id": "my-app-id@mozilla.org"
-      }
-    }
-  }
-}
+  firefox: {'applications': {'gecko': {'id': 'my-app-id@mozilla.org'}}}
+};
 
 
 // -----------------
 // COMMON
 // -----------------
-function js() {
-  return buildJS(target)
-}
-exports.js = js;
 
 function styles() {
   return gulp.src('src/styles/**/*.scss')
-    .pipe($.plumber())
-    .pipe($.sass.sync({
-      outputStyle: 'expanded',
-      precision: 10,
-      includePaths: ['.']
-    }).on('error', $.sass.logError))
-    .pipe(gulp.dest(`build/${target}/styles`));
+      .pipe($.plumber())
+      .pipe(
+          $.sass
+              .sync(
+                  {outputStyle: 'expanded', precision: 10, includePaths: ['.']})
+              .on('error', $.sass.logError))
+      .pipe(gulp.dest(`build/${target}/styles`));
 }
 exports.styles = styles;
 
 function manifest() {
   return gulp.src('./manifest.json')
-    .pipe(gulpif(!production, $.mergeJson({
-      fileName: "manifest.json",
-      jsonSpace: " ".repeat(4),
-      endObj: defaultManifest.dev
-    })))
-    .pipe(gulpif(target === "firefox", $.mergeJson({
-      fileName: "manifest.json",
-      jsonSpace: " ".repeat(4),
-      endObj: defaultManifest.firefox
-    })))
-    .pipe(gulp.dest(`./build/${target}`))
+      .pipe(gulpif(!production, $.mergeJson({
+        fileName: 'manifest.json',
+        jsonSpace: ' '.repeat(4),
+        endObj: defaultManifest.dev
+      })))
+      .pipe(gulpif(target === 'firefox', $.mergeJson({
+        fileName: 'manifest.json',
+        jsonSpace: ' '.repeat(4),
+        endObj: defaultManifest.firefox
+      })))
+      .pipe(dest(`./build/${target}`))
 }
 exports.manifest = manifest;
 
@@ -86,54 +72,49 @@ function pipe(src, ...transforms) {
   return transforms.reduce((stream, transform) => {
     const isDest = typeof transform === 'string'
     return stream.pipe(isDest ? gulp.dest(transform) : transform)
-  }, gulp.src(src))
+  }, gulp.src(src, { allowEmpty: true }))
 }
 
 function mergeAll(dest) {
   return merge(
-    pipe('./src/icons/**/*', `./build/${dest}/icons`),
-    pipe(['./src/_locales/**/*'], `./build/${dest}/_locales`),
-    pipe([`./src/images/${target}/**/*`], `./build/${dest}/images`),
-    pipe(['./src/images/shared/**/*'], `./build/${dest}/images`),
-    pipe(['./src/**/*.html'], `./build/${dest}`)
-  )
+      pipe('./src/icons/**/*', `./build/${dest}/icons`),
+      pipe(['./src/_locales/**/*'], `./build/${dest}/_locales`),
+      pipe([`./src/images/${target}/**/*`], `./build/${dest}/images`),
+      pipe(['./src/images/shared/**/*'], `./build/${dest}/images`),
+      pipe(['./src/**/*.html'], `./build/${dest}`))
 }
 
 function buildJS(target) {
-  const files = [
-    'background.js',
-    'contentscript.js',
-    'options.js',
-    'popup.js',
-    'livereload.js'
-  ]
+  const files =
+      [
+        'background.js', 'contentscript.js', 'options.js', 'popup.js',
+        'livereload.js'
+      ]
 
-  let tasks = files.map(file => {
-    return browserify({
-      entries: 'src/scripts/' + file,
-      debug: true
-    })
-      .transform('babelify', { presets: ['es2015'] })
-      .transform(preprocessify, {
-        includeExtensions: ['.js'],
-        context: context
-      })
-      .bundle()
-      .pipe(source(file))
-      .pipe(buffer())
-      .pipe(gulpif(!production, $.sourcemaps.init({ loadMaps: true })))
-      .pipe(gulpif(!production, $.sourcemaps.write('./')))
-      .pipe(gulpif(production, $.uglify({
-        "mangle": false,
-        "output": {
-          "ascii_only": true
-        }
-      })))
-      .pipe(gulp.dest(`build/${target}/scripts`));
-  });
+      let tasks = files.map(file => {
+        return browserify({entries: 'src/scripts/' + file, debug: true})
+            .transform('babelify', {presets: ['@babel/preset-env']})
+            .transform(
+                preprocessify, {includeExtensions: ['.js'], context: context})
+            .bundle()
+            .pipe(source(file))
+            .pipe(buffer())
+            .pipe(gulpif(!production, $.sourcemaps.init({loadMaps: true})))
+            .pipe(gulpif(!production, $.sourcemaps.write('./')))
+            .pipe(gulpif(
+                production,
+                $.uglify({'mangle': false, 'output': {'ascii_only': true}})))
+            .pipe(gulp.dest(`build/${target}/scripts`));
+      });
 
   return merge.apply(null, tasks);
 }
+
+function js(done) {
+  buildJS(target)
+  done()
+}
+exports.js = js;
 
 // Overall
 
@@ -141,23 +122,26 @@ function buildJS(target) {
 function clean() {
   return pipe(`./build/${target}`, $.clean())
 }
+exports.clean = clean;
 
 function watch() {
   $.livereload.listen();
 
-  gulp.watch(['./src/**/*']).on("change", () => {
+  gulp.watch(['./src/**/*']).on('change', () => {
     $.runSequence('build', $.livereload.reload);
   });
 }
-
-function ext() {
-  return mergeAll(target)
-}
-
-exports.clean = clean;
 exports.watch = watch;
-exports.assets = parallel(styles, js);
-exports.build = series(clean, exports.assets, ext);
+
+function ext(done) {
+  mergeAll(target)
+  done()
+}
+exports.ext = ext
+
+// exports.assets = gulp.parallel(exports.styles, exports.js);
+exports.assets = gulp.series(exports.styles, exports.js);
+exports.build = gulp.series(clean, exports.assets, ext);
 exports.default = exports.build;
-exports.dist = series(exports.build, zip);
-exports.ext = series(manifest, js, ext);
+exports.dist = gulp.series(exports.build, zip);
+exports.ext = gulp.series(manifest, exports.js, ext);
